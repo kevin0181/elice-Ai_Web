398cd2ddb62ded21a56c866e9296445865e03e57
kevin <kevin0181@naver.com>
2022-07-17 00:33:31 +0900

    게시글 생성 후 리다이렉트까지 성공

--- prac/view/static/js/postsInerts.js
+++ prac/view/static/js/postsInerts.js
@@ -10,5 +10,15 @@ const insertContent = () => {
         return;
     }
 
-    $("#contentForm").submit();
+    let formData = $('#contentForm').serialize(); //serialize는 form 태그내의 항목들을 자동으로 읽어와 queryString형으로 변경해준다.
+    $.ajax({
+        type: "POST",
+        url: 'http://localhost:8080/posts/',
+        data: formData,
+        success: (res) => {
+            if (res.result === 'save-success') {
+                location.href = "./../posts/list.html";
+            }
+        }
+    })
 }
