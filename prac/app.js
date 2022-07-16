cf7da46926d9d53e76fe81e36f9584001d0fbf5f
kevin <kevin0181@naver.com>
2022-07-16 17:55:51 +0900

    변경

--- prac/app.js
+++ prac/app.js
@@ -3,6 +3,18 @@ const app = express();
 const dayjs = require('dayjs');
 const postsRouter = require("./routes/posts")
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myapp');
+
+mongoose.connection.on("error", (err) => {
+    console.log(err);
+});
+
+mongoose.connection.on('connected', () => {
+    console.log("Database Connection Success");
+});
+
+
 app.use("/posts", postsRouter);
 
 app.locals.formatDate = (date) => {
