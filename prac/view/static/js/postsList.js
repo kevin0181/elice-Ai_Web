bb7e3d38682fd7f6ca1f2504c96b0bd133419189
kevin <kevin0181@naver.com>
2022-07-16 22:52:38 +0900

    데이터 가져와서 리스트 만들기 성공

--- prac/view/static/js/postsList.js
+++ prac/view/static/js/postsList.js
@@ -5,6 +5,23 @@ $(document).ready(() => {
         url: "http://localhost:8080/posts",
         success: (res) => {
             console.log(res); //값 가져옴
+            res.map((it, index) => {
+
+                let listData = `<tr>
+                <th scope="row">${index+1}</th>
+                <td>${it.title}</td>
+                <td>elice</td>
+                <td>
+                <button type="button" class="btn btn-outline-danger">삭제</button>
+                <button type="button" class="btn btn-outline-info">수정</button>
+                </td>
+                </tr>`
+
+                console.log(listData);
+
+
+                $(".postsList").append(listData);
+            })
         }
     })
 
