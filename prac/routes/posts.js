f2e4a80d8b105a39cea2179b2a951bb2c2caa33c
kevin <kevin0181@naver.com>
2022-07-18 21:22:24 +0900

    jwt 토큰 성공

--- prac/routes/posts.js
+++ prac/routes/posts.js
@@ -11,8 +11,6 @@ const authMiddleware = require("../utils/authMiddleware")
 //     }
 // });
 
-router.use(authMiddleware);
-
 router.post('/', async (req, res, next) => { // 게시글 작성
     const {title, content, author} = req.body;
     try {
@@ -27,7 +25,7 @@ router.post('/', async (req, res, next) => { // 게시글 작성
     }
 });
 
-router.get("/", async (req, res, next) => { //전체 게시글 목록을 전달해줌
+router.get("/", authMiddleware, async (req, res, next) => { //전체 게시글 목록을 전달해줌
     console.log(req.query);
     if (req.query.page < 1) {
         next("Please enter a number greater than 1"); //page가 0보다 작으면 오류
