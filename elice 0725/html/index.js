8539540ced8144e66a395470b4c61ef5e8a87f3a
kevin <kevin0181@naver.com>
2022-07-20 13:01:40 +0900

    만드는중

--- elice 0725/html/index.js
+++ elice 0725/html/index.js
@@ -2,8 +2,17 @@ $("#btn").click(() => {
     let value = $("#content-input").val();
 
     let list = `<li>${value} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <button>삭제</button>
-    <button>완료</button></li>`;
+    <button onclick="deleteLine(this)">삭제</button>
+    <button onclick="successLine(this)">완료</button></li><br/>`;
 
     $("#content").append(list);
 })
+
+const deleteLine = (data) => {
+
+}
+
+
+const successLine = (data) => {
+    $(data).parents("li").addClass("line");
+}
