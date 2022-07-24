0b8fc0a98bc8b9d03b69d2b62dbcd2f55bf75567
kevin <kevin0181@naver.com>
2022-07-24 20:23:09 +0900

    detail페이지 생성 예정

--- elice 0729(react 실습)/src/ReviewCreate.js
+++ elice 0729(react 실습)/src/ReviewCreate.js
@@ -6,8 +6,8 @@ function ReviewCreate() {
             <section className="py-5 text-center container">
                 <div className="row py-lg-5">
                     <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light">Moview</h1>
-                        <p className="lead text-muted">리뷰하고 싶은 영화를 추가하고, 별점을 주세요.<br /> 또한 삭제 수정이 가능합니다.</p>
+                        <h1 className="fw-light">Movie</h1>
+                        <p className="lead text-muted">리뷰하고 싶은 영화를 추가하고, 별점을 주세요.<br /> Create Page</p>
                     </div>
                 </div>
             </section>
