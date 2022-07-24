7cdb3d7d980967fd695323768cf56269faf4be88
kevin <kevin0181@naver.com>
2022-07-24 17:36:53 +0900

    디자인 일부분 한글로 변경

--- elice 0729(react 실습)/src/Review.js
+++ elice 0729(react 실습)/src/Review.js
@@ -5,11 +5,11 @@ const Review = () => {
                 <section className="py-5 text-center container">
                     <div className="row py-lg-5">
                         <div className="col-lg-6 col-md-8 mx-auto">
-                            <h1 className="fw-light">Album example</h1>
-                            <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                            <h1 className="fw-light">Moview</h1>
+                            <p className="lead text-muted">리뷰하고 싶은 영화를 추가하고, 별점을 주세요.<br /> 또한 삭제 수정이 가능합니다.</p>
                             <p>
-                                <a href="#" className="btn btn-primary my-2">Main call to action</a>
-                                <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+                                <a href="#" className="btn btn-primary my-2">Create Review</a>
+                                {/* <a href="#" className="btn btn-secondary my-2"></a> */}
                             </p>
                         </div>
                     </div>
