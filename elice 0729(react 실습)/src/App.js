cf132556b37eb82f3fedd8d766290ab2aa1bbd77
kevin <kevin0181@naver.com>
2022-07-24 19:41:11 +0900

    로그인 폼생성할거

--- elice 0729(react 실습)/src/App.js
+++ elice 0729(react 실습)/src/App.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 function App() {
+
+  const [loginView, setLoginView] = useState(false);
+
   return (
     <div className="App">
-      
+      <main>
+        <section className="py-5 text-center container">
+          <div className="row py-lg-5">
+            <div className="col-lg-6 col-md-8 mx-auto">
+              <h1 className="fw-light">Moview</h1>
+              <p className="lead text-muted">리뷰하고 싶은 영화를 추가하고, 별점을 주세요.<br /> 또한 삭제 수정이 가능합니다.</p>
+              <p>
+                <a href="#" className="btn btn-primary my-2">로그인</a>
+              </p>
+            </div>
+          </div>
+        </section>
+        <>
+          
+        </>
+      </main>
     </div>
   );
 }
---
+++ elice 0729(react 실습)/src/pages/user/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+    return (
+        <div className="album py-5 bg-light">
+            <div className="container">
+                로그인폼
+            </div>
+        </div>
+    );
+}
+
+export default Login;
