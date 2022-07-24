8366ebd4e16451aa87c980822ebb8618fa69e661
kevin <kevin0181@naver.com>
2022-07-24 17:50:36 +0900

    기본적인 디자인 끝

--- elice 0729(react 실습)/src/Review.js
+++ elice 0729(react 실습)/src/Review.js
@@ -31,8 +31,8 @@ const Review = () => {
                                         <p className="card-text">세계 최고의 슈퍼 악당을 꿈꾸는 미니보스 ‘그루’와 그를 따라다니는 미니언들. 어느 날 그루는 최고의 악당 조직 ‘빌런6’의 마법 스톤을 훔치는데 성공하지만 뉴페이스 미니언 ‘오토’의 실수로 스톤을 잃어버리고 빌런6에게 납치까지 당한다. 미니보스를 구하기 위해 잃어버린 스톤을 되찾아야 하는 ‘오토’, 그리고 쿵푸를 마스터해야 하는 ‘케빈’, ‘스튜어트’, ‘밥’! 올여름 극장가를 점령할 MCU(미니언즈 시네마틱 유니버스)가 돌아온다!</p>
                                         <div className="d-flex justify-content-between align-items-center">
                                             <div className="btn-group">
-                                                <button type="button" className="myBtn-red btn btn-sm btn-outline-secondary">삭제</button>
-                                                <button type="button" className="myBtn-yellow btn btn-sm btn-outline-secondary">수정</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">삭제</button>
+                                                <button type="button" className="btn btn-sm btn-outline-secondary">수정</button>
                                             </div>
                                             <small className="text-muted">9 mins</small>
                                         </div>
