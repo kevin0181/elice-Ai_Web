5d66cedbb9f8aea403557c2859a7a6d5751489f0
kevin <kevin0181@naver.com>
2022-07-24 19:54:02 +0900

    회원가입 폼 완성

--- elice 0729(react 실습)/src/pages/user/SignUp.js
+++ elice 0729(react 실습)/src/pages/user/SignUp.js
@@ -1,8 +1,26 @@
 const SignUp = () => {
     return (
-        <div className="album py-5 bg-light">
+        <div className="album bg-light">
             <div className="container">
-                회원가입폼
+                <form>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name={"email"} aria-describedby="emailHelp" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" name={"password"} id="password" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Re - Password</label>
+                        <input type="password" class="form-control" name={"password"} id="password" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" name={"name"} id="name" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">SignUp</button>
+                </form>
             </div>
         </div>
     );
