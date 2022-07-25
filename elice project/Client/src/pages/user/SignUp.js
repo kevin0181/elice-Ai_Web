385aa998c704c13c2ba6284056f9586eed95c848
kevin <kevin0181@naver.com>
2022-07-25 11:14:44 +0900

    for오류 className오류 해결

--- elice project/Client/src/pages/user/SignUp.js
+++ elice project/Client/src/pages/user/SignUp.js
@@ -1,25 +1,25 @@
 const SignUp = () => {
     return (
-        <div className="album bg-light">
+        <div className="album">
             <div className="container">
                 <form>
-                    <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name={"email"} aria-describedby="emailHelp" />
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email" name={"email"} aria-describedby="emailHelp" />
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" name={"password"} id="password" />
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" name={"password"} id="password" />
                     </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Re - Password</label>
-                        <input type="password" class="form-control" name={"password"} id="password" />
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Re - Password</label>
+                        <input type="password" className="form-control" name={"password"} id="password" />
                     </div>
-                    <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
-                        <input type="text" class="form-control" name={"name"} id="name" />
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" name={"name"} id="name" />
                     </div>
-                    <button type="submit" class="btn btn-primary">SignUp</button>
+                    <button type="submit" className="btn btn-primary">SignUp</button>
                 </form>
             </div>
         </div>
