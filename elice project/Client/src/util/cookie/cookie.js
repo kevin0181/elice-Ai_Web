98bc284706c4b660e366afedfd21b4e60c832c3b
kevin <kevin0181@naver.com>
2022-07-25 22:48:03 +0900

    정상적으로 값 가져올 수 있음

---
+++ elice project/Client/src/util/cookie/cookie.js
@@ -0,0 +1,10 @@
+import { Cookies } from "react-cookie";
+const cookies = new Cookies();
+
+export const setCookie = (key, data, path) => {
+    return cookies.set(key, data, { ...path });
+}
+
+export const getCookie = (key) => {
+    return cookies.get(key);
+}
