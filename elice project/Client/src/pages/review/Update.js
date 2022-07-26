ab568777e6c3e0f518f2c5f962b8cb97e37a0b87
kevin <kevin0181@naver.com>
2022-07-26 20:27:07 +0900

    업데이트하는 부분에서 데이터 가져오기 성공

--- elice project/Client/src/pages/review/Update.js
+++ elice project/Client/src/pages/review/Update.js
@@ -1,12 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
+import url from "./../../data/serverUrl.json";
+import { useCookies } from 'react-cookie';
 
 const Update = () => {
 
+    const [cookies, setCookie, removeCookie] = useCookies(["tokenData"]);
+
+    const [updateReviewData, setUpdateReviewData] = useState({});
+
     const data = useSelector(state => state.user.value);
 
+    const findUpdateReviewData = async (shortId) => {
+        return await axios.get(url.url + `/posts/${shortId}/find`, {
+            headers: {
+                accessToken: cookies.tokenData.accessToken
+            }
+        })
+    }
+
     useEffect(() => {
-        console.log(data);
+        findUpdateReviewData(data).then((res) => {
+            setUpdateReviewData(res.data);
+        })
     }, [])
 
     return (
@@ -15,22 +32,22 @@ const Update = () => {
                 <form>
                     <div className="card mb-3">
                         <div className="card-img-top" style={{ textAlign: 'center' }}>
-                            <img src="https://search.pstatic.net/common?type=o&size=174x246&quality=100&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20220720_283%2F1658284839003UzxoT_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2" alt="..." />
+                            <img src={updateReviewData.url} alt="..." />
                         </div>
                         <div className="card-body">
                             <h5 className="card-title">Movie Img</h5>
                             <p className="card-text">Img Example</p>
-                            <input type="text" className="form-control" id="title" name={"title"} placeholder="Input Img Url" />
+                            <input type="text" className="form-control" id="title" defaultValue={updateReviewData.url} name={"title"} placeholder="Input Img Url" />
                             <p className="card-text"><small className="text-muted">https://search.pstatic.net/common?type=o&size=174x246&q ...</small></p>
                         </div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control" id="title" name={"title"} placeholder="Movie Title" />
+                        <input type="text" className="form-control" id="title" defaultValue={updateReviewData.title} name={"title"} placeholder="Movie Title" />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="content" className="form-label">Content</label>
-                        <textarea className="form-control" id="content" name="content" rows="3"></textarea>
+                        <textarea className="form-control" id="content" defaultValue={updateReviewData.content} name="content" rows="3"></textarea>
                     </div>
                     <button type="button" className="btn btn-outline-primary m-3">Update</button>
                     <button type="button" className="btn btn-outline-danger" onClick={() => {
