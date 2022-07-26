b39a7877f121a5c9871beea2548a5a92dd862a9f
kevin <kevin0181@naver.com>
2022-07-26 19:57:37 +0900

    redux사용하기전

--- elice project/Client/src/app/Store.js
+++ elice project/Client/src/app/Store.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { configureStore } from '@reduxjs/toolkit';
-import User from './reducer/User';
+import Data from './reducer/Data';
 
 // 삭당에다가 접시 등록하기
 export default configureStore({
     reducer: {
-        user: User,
+        user: Data,
     },
 });
