c11c73e34358b6b16d3dbc4bb1806e4c1a81d33c
kevin <kevin0181@naver.com>
2022-07-26 16:41:59 +0900

    권한별로 라우터 조정하는거하면댐

--- elice project/Client/src/App.js
+++ elice project/Client/src/App.js
@@ -12,10 +12,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import ReviewUpdate from "./ReviewUpdate";
 import Login from "./Login";
+import { useCookies } from "react-cookie";
 import { useEffect } from "react";
 
-
 function App() {
+
+  const [cookies, setCookie, removeCookie] = useCookies(["tokenData"]);
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <>
       <Header />
